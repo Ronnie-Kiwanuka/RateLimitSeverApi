3e2274bdb6f14cebf3585c36e923de27fcf9413a
Ronnie Kiwanuka <Ronnie Kiwanuka@RONNIE-KIWANUKA>
2022-09-21 11:26:03 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+using AspNetCoreRateLimit;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddMemoryCache();
+builder.Services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+builder.Services.AddInMemoryRateLimiting();
+
+builder.Services.Configure<IpRateLimitOptions>(options =>
+{
+    options.EnableEndpointRateLimiting = true;
+    options.StackBlockedRequests = false;
+    options.HttpStatusCode = 429;
+    options.ClientIdHeader = "Client-Id";
+    options.GeneralRules = new List<RateLimitRule>
+        {
+            new RateLimitRule
+            {
+                Endpoint = "*",
+                Period = "60s",
+                Limit = 2
+            }
+        };
+});
+
+var app = builder.Build();
+//app.UseIpRateLimiting();
+app.UseClientRateLimiting();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
